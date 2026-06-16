fe98303dea4edfc05f365a797c61f5d3d4450f77
gustavjung01 <gustavjung01@gmail.com>
2026-06-16 18:07:43 +0700

    Enable trusted host for auth

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import Google from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
